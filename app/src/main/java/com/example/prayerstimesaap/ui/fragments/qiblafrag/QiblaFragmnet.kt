8aa27341aa2ac6dc88d111726ea809343a088a9b
MostafaOwies <mostafaowies@yahoo.com>
2023-05-28 21:02:02 +0300

    Add Location to Qibla Fragment

--- app/src/main/java/com/example/prayerstimesaap/ui/fragments/qiblafrag/QiblaFragmnet.kt
+++ app/src/main/java/com/example/prayerstimesaap/ui/fragments/qiblafrag/QiblaFragmnet.kt
@@ -1,6 +1,10 @@
 package com.example.prayerstimesaap.ui.fragments.qiblafrag
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -10,21 +14,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import com.example.prayerstimesaap.MainActivity
-import com.example.prayerstimesaap.R
 import com.example.prayerstimesaap.databinding.FragmentQiblaFragmnetBinding
 import com.example.prayerstimesaap.utils.CompassManager
 import com.example.prayerstimesaap.utils.Resource
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.lang.Exception
+import java.util.Locale
 import javax.inject.Inject
 import kotlin.math.roundToInt
 
 
 @AndroidEntryPoint
 @RequiresApi(Build.VERSION_CODES.O)
+@SuppressLint("MissingPermission")
 class QiblaFragmnet : Fragment() {
 
 
@@ -34,6 +42,8 @@ class QiblaFragmnet : Fragment() {
 
     private lateinit var viewModel: QiblaViewModel
     private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,10 +62,12 @@ class QiblaFragmnet : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as MainActivity).qiblaViewModel
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         setupCompassRotation()
         Log.d(TAG ,"Qibla ")
         getQibla()
+        getLocation()
 
         compassManager.start()
 
@@ -88,6 +100,33 @@ class QiblaFragmnet : Fragment() {
                 e.stackTrace
             }
         }
+    }
+
+    private fun getLocation() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    val geocoder = Geocoder(requireContext(), Locale.getDefault())
+                    coroutineScope.launch {
+                        try {
+                            val addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+                            val country = addresses?.let { getCountryName(it) }
+                            binding?.locationTtv?.text = country
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+    }
+    private fun getCountryName(addresses: List<Address>): String? {
+        if (addresses.isNotEmpty()) {
+            val address = addresses[0]
+
+            return address.subAdminArea
+        }
+        return null
     }
 
     private fun setupCompassRotation() {
